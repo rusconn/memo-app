c6c14edc92bd0f04c26b18f6e14085b35e817095
rusconn <rusconn3@gmail.com>
2022-10-22 15:57:36 +0000

    feat: implement `search memos`

--- src/components/layout/ToolbarSearch.tsx
+++ src/components/layout/ToolbarSearch.tsx
@@ -1,5 +1,9 @@
-import { ComponentProps, memo, useCallback, useState } from "react";
+import Router, { useRouter } from "next/router";
+import { ComponentProps, memo, useCallback, useEffect, useState } from "react";
 import { TfiSearch } from "react-icons/tfi";
+
+import { pagesPath } from "@/$path";
+import { useClearSelectedMemoId } from "@/contexts";
 
 type Props = Pick<ComponentProps<"input">, "value" | "placeholder" | "onChange">;
 
@@ -20,13 +24,46 @@ const StyledComponent = ({ value, placeholder, onChange }: Props) => (
 export const Component = memo(StyledComponent);
 
 const Container = () => {
+  const router = useRouter();
   const [value, setValue] = useState("");
+  const [timerId, setTimerId] = useState<number>();
+  const clearSelectedMemoId = useClearSelectedMemoId();
+
+  const q = router.query.q as string | undefined;
+
+  // URL から検索テキストを復元する
+  useEffect(() => {
+    if (q) {
+      setValue(q);
+    }
+  }, [q]);
+
+  // 別のページへ遷移したら検索タイマーをクリアする
+  useEffect(() => {
+    return () => window.clearTimeout(timerId);
+  }, [timerId, router]);
 
   const placeholder = "メモを検索する";
 
-  const onChange: NonNullable<Props["onChange"]> = useCallback(({ currentTarget }) => {
-    setValue(currentTarget.value);
-  }, []);
+  const onChange: NonNullable<Props["onChange"]> = useCallback(
+    ({ currentTarget }) => {
+      window.clearTimeout(timerId);
+
+      setValue(currentTarget.value);
+
+      const id = window.setTimeout(() => {
+        void Router.push({
+          pathname: pagesPath.$url().pathname,
+          query: { q: currentTarget.value },
+        });
+
+        clearSelectedMemoId();
+      }, 500);
+
+      setTimerId(id);
+    },
+    [timerId, setValue, clearSelectedMemoId]
+  );
 
   return <Component {...{ value, placeholder, onChange }} />;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
+import Fuse from "fuse.js";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import { MemoCards } from "@/components/common";
 import { useFolders } from "@/contexts";
 
+// 下記 OptionalQuery を定義したいが、生成コードの import パスが間違っているので避けた
+// https://github.com/aspida/pathpida#define-query---nextjs
+
 const Page: NextPage = () => {
-  const memos = useFolders().flatMap(folder => folder.memos);
+  const router = useRouter();
+  const allMemos = useFolders().flatMap(({ memos }) => memos);
+
+  const q = router.query.q as string | undefined;
+
+  const [title, memos] =
+    !q || q === ""
+      ? ["すべてのメモ", allMemos]
+      : [
+          "検索結果",
+          new Fuse(allMemos, { keys: ["content", "folderName"], threshold: 0.65 })
+            .search({ $or: [{ content: q }, { folderName: q }] })
+            .map(({ item }) => item),
+        ];
 
   return (
     <>
       <Head>
-        <title>すべてのメモ</title>
+        <title>{title}</title>
       </Head>
       <div className="p-6">
         <MemoCards {...{ memos }} />
